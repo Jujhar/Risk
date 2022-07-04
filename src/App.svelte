49171fdac4f24ecf50ab33d98dad63871c319c2b
Jujhar Singh <jujhar.pannu@gmail.com>
2022-07-03 18:31:51 -0700

    add unit selection

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,30 @@
 <script>
   import { onMount } from 'svelte';
   onMount(() => {
+    console.log("33");
   })
 
   let turnCount = 3;
+  let selectionTerritory = 'x';
+  let selection = [];
+
+  function handleClick() {
+    this.style.backgroundColor = 'green'
+    selection.push(this);
+	}
+
+  function deSelect() {
+    selection.forEach(item=>{
+      console.log('33');
+      console.log(item);
+      item.style.backgroundColor = '';
+    })
+    selection = [];
+  }
 
 </script>
 
-<svelte:window on:keydown={handleKeydown}/>
+<!--<svelte:window on:keydown={handleKeydown}/>-->
 
 <html data-theme="retro" lang="en">
 <body>
@@ -35,49 +52,68 @@
 
   <!-- turn count -->
   <div style="position: absolute; left:200px; top:690px;">
-  <big><big style="font-size: 4em;">{turnCount}</big></big>
+  <big><big style="font-size: 4em;">{turnCount}</big></big><br />
+  <span on:click={deSelect} class="clickable">🚫 Deselect Units</span>
   </div>
 
   <!-- Regions -->
   <!-- America -->
-  <div id="Alaska" style="position: absolute; height:200px; width:300px; left:0px; top:390px; background-color: rgba(250,20,20,0.2);">
+  <div id="Alaska" class="tile" style="position: absolute; height:200px; width:300px; left:0px; top:390px; background-color: rgba(250,20,20,0.2);">
   </div>
 
-  <div id="Northwest Territory" style="position: absolute; height:200px; width:400px; left:300px; top:420px; background-color: rgba(250,20,20,0.2);">
-    <div style="position: relative; left:2px; top:3px;">
+  <div id="Northwest Territory" class="tile" style="position: absolute; height:200px; width:400px; left:300px; top:420px; background-color: rgba(250,20,20,0.2);">
+    <span style="position:sticky; left:2px; top:3px;" on:click={handleClick}>
       <img src="general.webp" width=100 />
-    </div>
+    </span>
 
-    <div style="position: relative; left:4px; top:1px;">
+    <span style="position: sticky; left:4px; top:1px; flex: 19.33%;" on:click={handleClick}>
       <img src="pieces/artillery.png" width=100 />
-    </div>
+    </span>
 
-    <div style="position: relative; left:40px; top:-202px;">
+    <span style="position: sticky; left:4px; top:2px; flex: 19.33%;" on:click={handleClick}>
       <img src="pieces/unit.png" width=200 />
-    </div>
+    </span>
+    <span style="position: sticky; left:4px; top:2px; flex: 19.33%;" on:click={handleClick}>
+      <img src="pieces/unit.png" width=200 />
+    </span>
+    <span style="position: sticky; left:4px; top:2px; flex: 19.33%;" on:click={handleClick}>
+      <img src="pieces/unit.png" width=200 />
+    </span>
+    <span style="position: sticky; left:4px; top:2px; flex: 19.33%;" on:click={handleClick}>
+      <img src="pieces/unit.png" width=200 />
+    </span>
+    <span style="position: sticky; left:4px; top:2px; flex: 19.33%;" on:click={handleClick}>
+      <img src="pieces/unit.png" width=200 />
+    </span>
+    <span style="position: sticky; left:4px; top:2px; flex: 19.33%;" on:click={handleClick}>
+      <img src="pieces/unit.png" width=200 />
+    </span>
+    <span style="position: sticky; left:4px; top:2px; flex: 19.33%;" on:click={handleClick}>
+      <img src="pieces/unit.png" width=200 />
+    </span>
 
 
   </div>
 
-  <div id="Alberta" style="position: absolute; height:170px; width:310px; left:320px; top:630px; background-color: rgba(250,20,20,0.2);">
+  <div id="Alberta" class="tile" style="position: absolute; height:170px; width:310px; left:320px; top:630px; background-color: rgba(250,20,20,0.2);">
   </div>
 
-  <div id="Ontario" style="position: absolute; height:190px; width:250px; left:630px; top:630px; background-color: rgba(250,20,20,0.2);">
+  <div id="Ontario" class="tile" style="position: absolute; height:190px; width:250px; left:630px; top:630px; background-color: rgba(250,20,20,0.2);">
   </div>
 
-  <div id="Quebec" style="position: absolute; height:240px; width:250px; left:880px; top:630px; background-color: rgba(250,20,20,0.2);">
+  <div id="Quebec" class="tile" style="position: absolute; height:240px; width:250px; left:880px; top:630px; background-color: rgba(250,20,20,0.2);">
   </div>
 
-  <div id="Western United States" style="position: absolute; height:240px; width:250px; left:380px; top:810px; background-color: rgba(250,20,20,0.2);">
+  <div id="Western United States" class="tile" style="position: absolute; height:240px; width:250px; left:380px; top:810px; background-color: rgba(250,20,20,0.2);">
   </div>
 
-  <div id="Eastern United States" style="position: absolute; height:280px; width:250px; left:680px; top:840px; background-color: rgba(250,20,20,0.2);">
+  <div id="Eastern United States" class="tile" style="position: absolute; height:280px; width:250px; left:680px; top:840px; background-color: rgba(250,20,20,0.2);">
   </div>
 
-  <div id="Central America" style="position: absolute; height:180px; width:150px; left:410px; top:1090px; background-color: rgba(250,20,20,0.2);">
+  <div id="Central America" class="tile" style="position: absolute; height:180px; width:150px; left:410px; top:1090px; background-color: rgba(250,20,20,0.2);">
   </div>
 
-  <div id="Greenland" style="position: absolute; height:380px; width:450px; left:1010px; top:210px; background-color: rgba(250,20,20,0.2);">
+  <div id="Greenland" class="tile" style="position: absolute; height:380px; width:450px; left:1010px; top:210px; background-color: rgba(250,20,20,0.2);">
   </div>
   <!-- america -->
 
@@ -91,5 +127,15 @@ body {
   width: 3300px;
   background-image: url(board.jpg);
   background-size: auto;
+}
+
+.tile {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.clickable {
+  cursor: pointer;
 }
 </style>
